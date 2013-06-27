9ca7582b42e8f21264a616ed5bf4e48b8620f823
Jens Page <jens.page@datajockey.org>
2013-06-27 13:13:02 -0400

    JAXB JSON to Map adapter, not working & resorted to Method objects… saving for now, may come back to this

---
+++ src/main/java/com/playgrid/api/entity/adapter/Methods.java
@@ -0,0 +1,12 @@
+package com.playgrid.api.entity.adapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class Methods {
+	public List<MethodsEntry> entry = new ArrayList<MethodsEntry>();
+}
+
+
