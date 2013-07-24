a625a6b2606a81f1ace1946be34ce937124b5d0f
Jens Page <jens.page@datajockey.org>
2013-07-24 12:03:59 -0400

    converted username to user_id added suspended_until added Status enum

--- src/main/java/com/playgrid/api/entity/Player.java
+++ src/main/java/com/playgrid/api/entity/Player.java
@@ -14,9 +14,10 @@ public class Player {
 	public String name;
 	public URI url;
 	public ArrayList<String> permission_groups;
-	public String status;
-	public String username;
+	public Status status;
+	public String user_id;
 	public String reason;
+	public Date suspended_until;
 	public Date auth_timestamp;
 	public Boolean online;
 	public Integer unverified_days;
@@ -32,5 +33,45 @@ public class Player {
 		return this.name;
 	
 	}
+
+
+	/**
+	 * Status to communicate to games to determine player authorization
+	 * @author jpage
+	 *
+	 */
+	public static enum Status {
+
+		/*
+		 * Player is authorized for game
+		 */
+		AUTHORIZED, 
+
+		/*
+		 * Player is banned from game
+		 */
+		BANNED,
+		
+		/*
+		 * An error occurred
+		 */
+		ERROR, 
+		
+		/*
+		 * Player has no status or is anonymous
+		 */
+		NONE, 
+		
+		/*
+		 * Player is suspended from game
+		 */
+		SUSPENDED, 
+		
+		/*
+		 * Player has not verified PlayGrid account
+		 */
+		UNVERIFIED, 
+	}
+
 
 }
